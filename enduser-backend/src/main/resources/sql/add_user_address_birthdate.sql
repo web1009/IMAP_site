504639b97f604c5299952f7ce611f613384b085b
이지민 <dlwlals52@naver.com>
2026-02-22 19:59:01 +0900

    admin-back

--- enduser-backend/src/main/resources/sql/add_user_address_birthdate.sql
+++
@@ -1,6 +0,0 @@
--- USERS 테이블에 주소, 생년월일 컬럼 추가
--- 실행: mysql -u root -p DB명 < add_user_address_birthdate.sql
-
-ALTER TABLE USERS
-ADD COLUMN address VARCHAR(255) NULL AFTER phone_number,
-ADD COLUMN birth_date DATE NULL AFTER address;
